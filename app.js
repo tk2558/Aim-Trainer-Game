038f08b0b9fb05870ef6ddde65bcf7ac0337148d
tk2558 <113456253+tk2558@users.noreply.github.com>
2023-06-03 00:52:10 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+const squares = document.querySelectorAll('.square')
+const target = document.querySelector('.target')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#score')
+
+// Variables
+let hit = 0
+let total = 0
+let hitPosition
+let currentTime = 60
+let timerId = null
+let hitSound = new Audio('hit-sound.mp3')
+
+// pick random squares
+function randomSquare() {
+    squares.forEach(square => {
+        square.classList.remove('target')
+    })
+
+    let randomSquare = squares[Math.floor(Math.random() * 24)]
+    randomSquare.classList.add('target')
+    hitPosition = randomSquare.id
+    total++
+}
+
+// Action Event Listener
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id == hitPosition) {
+            hit++
+            hitSound.play()
+            score.textContent = hit
+            hitPosition = null
+        }
+    })
+})
+
+// move Target
+function moveTarget() {
+    timerId = setInterval(randomSquare, 500)
+}
+
+// Count Down
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if (currentTime == -1) {
+        alert('GAME OVER! Final score is ' + ((hit / total) * 100).toFixed(2) + '% accuracy')
+        clearInterval(countDownTimer)
+        clearInterval(timerId)
+        timeLeft.textContent = 0
+    }
+}
+
+moveTarget()
+let countDownTimer = setInterval(countDown, 1000) // countdown timer
